688970cd2c9f6e01572484ccdb842485b23031a9
mauroef <cypress90@gmail.com>
2015-04-22 01:10:24 -0300

    faltan funciones y test

--- Cuenta bancaria/ar.edu.tallerweb.cuenta/ar/edu/tallerweb/cuenta/CajaDeAhorro.java
+++ Cuenta bancaria/ar.edu.tallerweb.cuenta/ar/edu/tallerweb/cuenta/CajaDeAhorro.java
@@ -1,5 +1,25 @@
 package ar.edu.tallerweb.cuenta;
 
 public class CajaDeAhorro {
-
+	//declaracion de atributos
+	String numeroCuenta;
+	double saldo;
+	
+	public CajaDeAhorro(String valorNumeroCuenta, double valorSaldo){
+		//constructor
+		numeroCuenta=valorNumeroCuenta;
+		saldo=0;
+	}
+	//metodos
+	public void operacionDeposito(double saldo){
+		
+	}
+	
+	public void operacionExtraccion(double saldo){
+		
+	}
+	
+	public void operacionConsultaDeSaldo(double saldo){
+		
+	}
 }
